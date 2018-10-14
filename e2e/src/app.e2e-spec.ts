f6761d77b4f9e1d1cd3119e863d920cc534be3f2
paul altefogt <paltefogt@gmail.com>
2018-10-14 13:06:16 -0400

    first test

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -9,6 +9,6 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to bestOfAllTailors-portal!');
+    expect(page.getParagraphText()).toEqual('bestOfAllTailors-portal');
   });
 });
